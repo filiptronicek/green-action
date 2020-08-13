024fb43198a1732c82a1a018c338df3f68cb1a97
Filip Troníček <filip.tronicek@seznam.cz>
2020-08-14 00:45:42 +0200

    Artifact action and code for that

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 require("dotenv").config();
 const axios = require("axios");
+const artifact = require("@actions/artifact");
 
 const { URL: linkToScan } = process.env;
 
+const artifactUp = async () => {
+  const artifactClient = artifact.create();
+  const artifactName = "output";
+  const files = ["/output/output.json"];
+
+  const rootDirectory = "."; // Also possible to use __dirname
+  const options = {
+    continueOnError: false,
+  };
+
+  const results = await artifactClient.uploadArtifact(
+    artifactName,
+    files,
+    rootDirectory,
+    options
+  );
+  return results;
+};
 
 const main = async () => {
-    console.log(`👀 scanning ${linkToScan}`);
-    
-    const carbonData = await axios(`https://api.websitecarbon.com/site?url=${linkToScan}`);
-    console.log(carbonData.data);
+  console.log(`👀 scanning ${linkToScan}`);
+
+  const carbonData = await axios(
+    `https://api.websitecarbon.com/site?url=${linkToScan}`
+  );
+  console.log(carbonData.data);
 };
 
 (async () => {
