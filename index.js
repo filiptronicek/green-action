f604d533bde41ebe153c060572223eaa73c91bf6
Filip Troníček <filip.tronicek@seznam.cz>
2020-08-17 11:28:51 +0200

    Bump version and delete debug

--- index.js
+++ index.js
@@ -12,9 +12,8 @@ const fileName = "output.json";
 const getReadme = (percentage) => {
     const fileToEdit = 'README.md';
     fs.readFile(fileToEdit, 'utf8', function (err,data) {
-        if(err) {
+        if(err)
             return console.log(err);
-        }
         let toWrite;
         if (data.includes("<!-- CARBON-STATS -->")) {
           toWrite = data.replace("<!-- CARBON-STATS -->", `![carbon consumption of this project](https://green-action.vercel.app/api/card?p=${percentage})`);
@@ -22,7 +21,6 @@ const getReadme = (percentage) => {
           const r = new RegExp("\/api\/card\\?p=[0-9]{1,3}", "g");
           toWrite = data.replace(r, `/api/card?p=${percentage}`);
         }
-        console.log(toWrite);
         fs.writeFile(fileToEdit, toWrite ,(err) => {
           if(err) return console.error(err); 
         });
